60d3ea2e46beeba30431e21e42a011ab5de355d7
Thinh5678 <tranchanhthinh2603@gmail.com>
2024-10-21 19:08:49 +0700

    Set up project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.pqhxw.mongodb.net',
+    DB_USER: 'tranchanhthinh2603',
+    DB_PASS: '123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET:"12345",
+    JWT_EXPIRES:'3600000', // in milliseconds
+    EMAIL_USER: "tranchanhthinh2603@gmail.com",
+    EMAIL_PASS: 'thinh2603'
+  };
+  module.exports = MyConstants;
